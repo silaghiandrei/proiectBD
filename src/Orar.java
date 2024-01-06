8b4d4b5b9300fb960805c0df021aa8627e1cf7ea
Andrei <silaghiandrei03@gmail.com>
2024-01-06 02:23:35 +0200

    modificari 6 ianuarie 2024 ora 2:23

---
+++ src/Orar.java
@@ -0,0 +1,10 @@
+import java.sql.Time;
+
+public class Orar {
+    public Integer idorar;
+    public Integer idAngajat;
+    public Time start;
+    public Time sfarsit;
+    public String zi;
+    public String denumirePoli;
+}
